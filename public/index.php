9bd2c597fed31d84eed578f4dc41d618c68c0d0e
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-03-25 21:48:46 +0800

    refactor: remove add() method to simplify

--- public/index.php
+++ public/index.php
@@ -6,11 +6,6 @@ use Core\Router;
 
 $router = new Router();
 
-$routes = require __DIR__ . '/../routes/web.php';
-
-foreach ($routes as $route => $handler) {
-    [$method, $path] = explode(' ', $route, 2);
-    $router->add($method, $path, $handler);
-}
+require __DIR__ . '/../routes/web.php';
 
 $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
