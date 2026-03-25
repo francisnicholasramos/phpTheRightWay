1440459a1035aebe71525752e902a52356ca96f7
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-03-26 02:32:36 +0800

    add posts, likes, comments table

--- sql.php
+++ sql.php
@@ -20,6 +20,42 @@ $sql = <<<SQL
         CONSTRAINT "users_username_key"  UNIQUE ("username"),
         CONSTRAINT "users_email_key"     UNIQUE ("email")
     );
+
+    CREATE TYPE "visibility_type" AS ENUM ('public', 'friends', 'private');
+    CREATE TABLE IF NOT EXISTS "posts" (
+        "id"        TEXT        NOT NULL DEFAULT gen_random_uuid(),
+        "content"   TEXT        NOT NULL,
+        "user_id"   TEXT        NOT NULL,
+        "created_at" TIMESTAMP(3)   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "visibility" visibility_type NOT NULL DEFAULT 'public',
+
+        FOREIGN KEY ("user_id")    REFERENCES "users"("id"),
+        
+        CONSTRAINT "posts_pkey"         PRIMARY KEY ("id")
+    );
+
+    CREATE TABLE IF NOT EXISTS "comments" (
+        "id"        TEXT        NOT NULL DEFAULT gen_random_uuid(),
+        "content"   TEXT        NOT NULL,
+        "user_id"   TEXT        NOT NULL,
+        "created_at" TIMESTAMP(3)   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "post_id"   TEXT        NOT NULL,
+        
+        FOREIGN KEY ("post_id")    REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+
+        CONSTRAINT "comments_pkey"         PRIMARY KEY ("id")
+    );
+
+    CREATE TABLE IF NOT EXISTS "likes" (
+        "id"        TEXT        NOT NULL DEFAULT gen_random_uuid(),
+        "user_id"   TEXT        NOT NULL,
+        "post_id"   TEXT        NOT NULL,
+
+        FOREIGN KEY ("user_id")    REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY ("post_id")    REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE,
+
+        CONSTRAINT "likes_pkey"    PRIMARY KEY ("id")
+    );
 SQL;
 
 try {
