03de86c894b371cc17cfd6edc847c029e328aca4
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-03-26 16:24:03 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,9 @@ use App\Controllers\AuthController;
 use App\Controllers\FeedController;
 use Core\Router;
 
-Router::get('/login', [AuthController::class, 'show']);
+Router::get('/login', [AuthController::class, 'loginPage']);
+Router::get('/signup', [AuthController::class, 'signUpPage']);
 Router::get('/feed', [FeedController::class, 'index'])->middleware('Authenticate');
+
+Router::post('/login', [AuthController::class, 'loginHandler']);
+Router::post('/signup', [AuthController::class, 'signUpHandler']);
