f5261616715853411d4a7816fcac990da7edd762
Francis Nicholas Ramos <pfrancisnicholasramos@gmail.com>
2026-05-01 23:52:43 +0800

    fix: show only to logged-user

--- resources/views/layouts/Sidebar.php
+++ resources/views/layouts/Sidebar.php
@@ -1,3 +1,5 @@
-<aside class="sidebar">
-    <?php require __DIR__ . '/../components/searchbar.php'; ?>
-</aside>
+<?php if (isset($_SESSION['user_id'])) : ?>
+    <aside class="sidebar">
+        <?php require __DIR__ . '/../components/searchbar.php'; ?>
+    </aside>
+<?php endif; ?>
